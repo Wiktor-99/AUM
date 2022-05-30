b4ce6e31a26a89d8f76748eadb7df3ce4369f3c2
Wiktor Bajor <wiktorbajor1@gmail.com>
2022-05-30 16:50:12 +0200

    Add main and models loading to main app

--- AnimalsImageRecognitionApp.py
+++ AnimalsImageRecognitionApp.py
@@ -2,20 +2,20 @@ from skimage.io import imread_collection
 from skimage.transform import resize
 import numpy as np
 from joblib import load
+from sklearn.metrics import accuracy_score
+import sys
 
-def load_images(class_name, values, labels):
-    path_to_image = 'raw-img/'
+
+def load_images(path_to_images, class_name, values, labels):
     postfix = '/*.jp*'
     image_size = (64,64,3)
-    i = 0
-    for image in imread_collection(path_to_image + class_name + postfix):
-
+    for image in imread_collection(path_to_images + class_name + postfix):
         img = resize(image, image_size)
         values.append(img)
         labels.append(class_name)
 
 
-def create_data_set():
+def create_data_set(path_to_images):
     class_names = ['spider', 'horse', 'elephant', 'chicken', 'sheep']
     values = []
     labels = []
@@ -23,3 +23,15 @@ def create_data_set():
         load_images(class_name, values, labels)
 
     return np.array(values), np.array(labels)
+
+def load_models():
+    return { "svm" : load("svm.sav"), "mlp" : load("mlp.sav"), "knn" : load("knn.sav")  }
+
+def main():
+    x, y = create_data_set(sys.argv[1])
+    models = load_models()
+    pred = models[sys.argv[2]].predict(x)
+    print('Percentage correct for model: ', 100*accuracy_score(pred, y))
+
+if __name__ == '__main__':
+    main()
