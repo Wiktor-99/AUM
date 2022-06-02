e3fd6937a02bb31b7ab4fc258a7707de73e98549
Wiktor Bajor <wiktorbajor1@gmail.com>
2022-06-02 21:53:27 +0200

    Add hog transform

--- AnimalsImageRecognitionApp.py
+++ AnimalsImageRecognitionApp.py
@@ -4,6 +4,11 @@ import numpy as np
 from joblib import load
 from sklearn.metrics import accuracy_score
 import sys
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from skimage.exposure import histogram
+from skimage.color import rgb2gray, rgb2hsv
+
 
 
 def load_images(path_to_images, class_name, values, labels):
@@ -20,16 +25,34 @@ def create_data_set(path_to_images):
     values = []
     labels = []
     for class_name in class_names:
-        load_images(class_name, values, labels)
+        load_images(path_to_images, class_name, values, labels)
 
     return np.array(values), np.array(labels)
 
 def load_models():
-    return { "svm" : load("svm.sav"), "mlp" : load("mlp.sav"), "knn" : load("knn.sav")  }
+    folder = "models/"
+    return { "svm" : load(folder + "svm.sav"),
+             "mlp" : load(folder + "mlp.sav"),
+             "knn" : load(folder + "knn.sav")  }
+
+def get_histogram(image, bins, channel):
+    hist, _ = histogram(rgb2hsv(image)[:, :, channel], nbins=bins)
+    return hist
+
+def get_all_colors_histograms(image, bins = 110):
+    return np.reshape((get_histogram(image, bins, 0), get_histogram(image, bins, 1), get_histogram(image, bins, 2)), (bins*3))
+
+def extract_hog_features(values):
+    scalify = StandardScaler()
+    return scalify.fit_transform(
+        np.array(
+            [ np.concatenate(
+                    (hog(rgb2gray(img), orientations = 5, pixels_per_cell = (8,8), cells_per_block = (8,8)), get_all_colors_histograms(img))) for img in values ]))
 
 def main():
     x, y = create_data_set(sys.argv[1])
     models = load_models()
+    x = extract_hog_features(x)
     pred = models[sys.argv[2]].predict(x)
     print('Percentage correct for model: ', 100*accuracy_score(pred, y))
 
